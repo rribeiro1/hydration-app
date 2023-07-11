6009b53f30f70164e87012c0285eb78157772e5f
Rafael Ribeiro <rafael.pr10@gmail.com>
2023-07-11 08:12:16 +0200

    Initial Commit

---
+++ Hydration/HydrationApp.swift
@@ -0,0 +1,17 @@
+//
+//  HydrationApp.swift
+//  Hydration
+//
+//  Created by Rafael Ribeiro on 11.07.23.
+//
+
+import SwiftUI
+
+@main
+struct HydrationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
